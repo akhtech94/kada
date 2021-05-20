4b5c66fa49233d894e854a1ec7129381e319ac6c
akhtech94 <akhtech94@gmail.com>
2021-05-20 21:44:07 +0530

    modified CustomUserAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,14 +3,29 @@ from django.contrib.auth.admin import UserAdmin
 from . models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
+    username = ''
+
     list_display = ('email', 'is_active', 'is_shop', 'is_delivery_person', 'is_customer')
+
     list_filter = ()
-    fieldsets = ()#UserAdmin.fieldsets + (
-    #     (None, {'fields': ('name',)}),
-    # )
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields': ( 'emai',)}),
+
+    # These override the definitions on the base UserAdmin
+    # that reference specific fields on auth.User.
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('name',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff','is_admin', 'is_shop', 'is_delivery_person', 'is_customer', )}),
     )
+
+    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
+    # overrides get_fieldsets to use this attribute when creating a user.
+    add_fieldsets = (
+        # (None, {
+            # 'classes': ('wide',),
+            # 'fields': ('email', 'name', 'password1', 'password2'),
+        # }),
+    )
+
     search_fields = ('email',)
     ordering = ('email',)
     filter_horizontal = ()
